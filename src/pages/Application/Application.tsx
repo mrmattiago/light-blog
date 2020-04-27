df70e56f9657a94eb74c63878ac390a0c391b5f6
Mat <mateusz.jach@gmail.com>
2020-04-27 22:10:37 +0200

    initial commit

---
+++ src/pages/Application/Application.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Application = () => {
+  return (
+    <div>
+      <header>header</header>
+      <main>content</main>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default Application;
